57e8549631addb8817bd0bd31c93871f4524cf2d
Beniamin <bbeniutek@gmail.com>
2021-12-10 16:53:16 +0000

    day 10 part 2 solution

--- day10.rb
+++ day10.rb
@@ -18,6 +18,13 @@ module Day10
       '>' => 25137
     }
 
+    COMPLETION_POINTS_MAP = {
+      ')' => 1,
+      ']' => 2,
+      '}' => 3,
+      '>' => 4
+    }
+
     CLOSING_CHARS = [']', ')', '}', '>']
 
     def part1
@@ -48,6 +55,52 @@ module Day10
     end
 
     def part2
+      input = load_input
+      opening_chars = []
+      expected_closing_chars = []
+      offending_chars = []
+      broken_line = false
+      points = []
+
+      input.each_with_index do |line, i|
+        line.each do |char|
+          if CLOSING_CHARS.include? char
+            opening_char = opening_chars.pop
+
+            if MAP[opening_char] != char
+              broken_line = true
+              break
+            end
+          else
+            opening_chars << char
+            expected_closing_chars << MAP[char]
+          end
+        end
+
+        if broken_line
+          offending_chars = []
+          expected_closing_chars = []
+          opening_chars = []
+          broken_line = false
+        else
+          closing_chars = []
+          opening_chars.reverse_each do |char|
+            closing_chars << MAP[char]
+          end
+          points << get_points(closing_chars)
+          opening_chars = []
+        end
+      end
+
+      {
+        points: points.sort.at(points.size/2)
+      }
+    end
+
+    def get_points(closing_chars)
+      closing_chars.reduce(0) do |acc, val|
+        acc * 5 + COMPLETION_POINTS_MAP[val]
+      end
     end
   end
 end
